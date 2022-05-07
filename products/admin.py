8e4f8eec4b43c6119bb15f87eaeefaa6cb938dde
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-07 16:18:27 +0000

    Amend admin display of product model fields & data

--- products/admin.py
+++ products/admin.py
@@ -3,8 +3,48 @@ from .models import Category, Size, Colour, Discount, Bag
 
 
 # Register your models here.
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+    ordering = ('friendly_name',)
+
+
+@admin.register(Colour)
+class ColourAdmin(admin.ModelAdmin):
+
+    ordering = ('name',)
+
+
+@admin.register(Discount)
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        '__str__',
+    )
+
+    ordering = ('amount',)
+
+
+@admin.register(Bag)
+class BagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'sku',
+        'category',
+        'size',
+        'colour',
+        'original_price',
+        'has_charm_option',
+        'overall_rating',
+        'on_sale',
+        'discount',
+    )
+
+    ordering = ('name',)
+
+
 admin.site.register(Size)
-admin.site.register(Colour)
-admin.site.register(Discount)
-admin.site.register(Bag)
