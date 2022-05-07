ea9ac5559f775c1154226b43fc62493b1688a1d0
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-07 15:30:13 +0000

    Add products app

--- saguaro_bags/settings.py
+++ saguaro_bags/settings.py
@@ -22,7 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -43,6 +42,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'home',
+    'products',
 ]
 
 MIDDLEWARE = [
