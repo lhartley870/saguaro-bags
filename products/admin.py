a79b31d45053995cb1ba919cc898a816ca8dd61f
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-07 15:33:02 +0000

    Register products app models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Category, Size, Colour, Discount, Bag
+
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Size)
+admin.site.register(Colour)
+admin.site.register(Discount)
+admin.site.register(Bag)
