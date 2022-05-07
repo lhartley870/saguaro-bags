a99cc3262931212d0cb4ac7ac43866ac76a9ea5e
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-07 16:42:09 +0000

    Add all_bags view to show all bags for sale

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Bag
+
 
 # Create your views here.
+def all_bags(request):
+    """ A view to show all bags, including sorting and search queries """
+    bags = Bag.objects.all()
+    context = {
+        'bags': bags,
+    }
+
+    return render(request, 'products/bags.html', context)
