94fa0461394323551a72b5e2689992dff3672884
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-09 15:31:58 +0000

    Add bag_detail view

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Bag
 
 
@@ -11,3 +11,13 @@ def all_bags(request):
     }
 
     return render(request, 'products/bags.html', context)
+
+
+def bag_detail(request, bag_id):
+    """ A view to show individual bag details """
+    bag = get_object_or_404(Bag, pk=bag_id)
+    context = {
+        'bag': bag,
+    }
+
+    return render(request, 'products/bag_detail.html', context)
