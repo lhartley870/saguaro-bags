1d2a35949390859efd3b8f18d60ae697a8fbd1d0
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-09 10:29:24 +0000

    Add method to calculate discounted prices of bags

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
@@ -65,3 +66,30 @@ class Bag(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_discounted_price(self):
+        """
+        Method to calculate the discounted price of a bag if it is on sale.
+
+        A check is performed in the bags.html template to check for bags where
+        on_sale is True but the discount is None (to cover any human error
+        where the admin may have marked the bag as on_sale but has failed to
+        complete the discount field) to ensure such bags are not displayed as
+        being on sale with the original_price being struck through. The if
+        statement in this method is a back up to the template check. If the
+        discount is not None, the new price is calculated to 2 decimal places,
+        otherwise the original_price is returned.
+        """
+        if (self.discount) is not None:
+            # Use of the Decimal module and the quantize method to calculate
+            # the discounted price and return it to 2 decimal places taken from
+            # the Python documentation -
+            # https://docs.python.org/3/library/decimal.html#decimal-faq
+            discount = (Decimal(self.discount.amount / 100)
+                        * self.original_price)
+            discounted_price = self.original_price - discount
+            two_places = Decimal(10) ** -2
+
+            return discounted_price.quantize(two_places)
+        else:
+            return self.original_price
