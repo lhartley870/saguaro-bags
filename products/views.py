9151fd31cc5acb00758a769efef8af7cafc12535
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-09 17:19:02 +0000

    Add to bag_detail view to work out rating stars

--- products/views.py
+++ products/views.py
@@ -16,8 +16,25 @@ def all_bags(request):
 def bag_detail(request, bag_id):
     """ A view to show individual bag details """
     bag = get_object_or_404(Bag, pk=bag_id)
+    rating_stars = bag.get_number_rating_stars()
+    if isinstance(rating_stars, list):
+        whole_stars = rating_stars[0]
+        whole_stars_list = list(range(1, rating_stars[0] + 1))
+        half_stars_list = [1]
+        empty_stars = 4 - whole_stars
+        empty_stars_list = list(range(1, empty_stars + 1))
+    else:
+        whole_stars = rating_stars
+        whole_stars_list = list(range(1, rating_stars + 1))
+        half_stars_list = []
+        empty_stars = 5 - whole_stars
+        empty_stars_list = list(range(1, empty_stars + 1))
+    
     context = {
         'bag': bag,
+        'whole_stars': whole_stars_list,
+        'half_stars': half_stars_list,
+        'empty_stars': empty_stars_list,
     }
 
     return render(request, 'products/bag_detail.html', context)
