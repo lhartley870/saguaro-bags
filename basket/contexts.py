7bbc181b6e79f7073fc268c9a5b32a5f52c585c5
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-15 14:24:52 +0000

    Add basket contents to basket_contents context

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Bag
 
 
 def basket_contents(request):
@@ -7,6 +9,22 @@ def basket_contents(request):
     basket_items = []
     total = 0
     bag_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        bag = get_object_or_404(Bag, pk=item_id)
+        # If the bag is on sale, use the discounted price.
+        if bag.on_sale and bag.discount is not None:
+            total += quantity * bag.get_discounted_price()
+        # If the bag is not on sale use the original price.
+        else:
+            total += quantity * bag.original_price
+        bag_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'bag': bag,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
