1ff96a47515c1dedf64a6cfb3511efa2eb26125a
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-16 15:17:25 +0000

    Add to add_to_basket view to deal with bag charms

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Bag
 
 
 # Create your views here.
@@ -13,12 +14,29 @@ def add_to_basket(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    charm = None
     basket = request.session.get('basket', {})
 
-    if item_id in list(basket.keys()):
-        basket[item_id] += quantity
+    if 'charm_option' in request.POST:
+        charm = request.POST['charm_option']
+
+    bag = get_object_or_404(Bag, pk=item_id)
+    if not charm and bag.has_charm_option:
+        charm = 'No charm selected'
+
+    if charm:
+        if item_id in list(basket.keys()):
+            if charm in basket[item_id]['items_by_charm_option'].keys():
+                basket[item_id]['items_by_charm_option'][charm] += quantity
+            else:
+                basket[item_id]['items_by_charm_option'][charm] = quantity
+        else:
+            basket[item_id] = {'items_by_charm_option': {charm: quantity}}
     else:
-        basket[item_id] = quantity
+        if item_id in list(basket.keys()):
+            basket[item_id] += quantity
+        else:
+            basket[item_id] = quantity
 
     request.session['basket'] = basket
 
