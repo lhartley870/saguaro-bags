a64bc4ed60ce73cb8843d9ced154b1f88ccb9eb5
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-17 14:11:15 +0000

    Add comments to add_to_basket view in basket app

--- basket/views.py
+++ basket/views.py
@@ -17,24 +17,39 @@ def add_to_basket(request, item_id):
     charm = None
     basket = request.session.get('basket', {})
 
+    # If a charm option has been selected, charm
+    # is set to that charm's id.
     if 'charm_option' in request.POST:
         charm = request.POST['charm_option']
 
+    # If the bag has a charm option but no charm has
+    # been selected, charm is set to 'No charm selected'.
     bag = get_object_or_404(Bag, pk=item_id)
     if not charm and bag.has_charm_option:
         charm = 'No charm selected'
 
+    # If the submitted bag has a charm option.
     if charm:
         if item_id in list(basket.keys()):
+            # If the submitted bag and charm combination is already in
+            # the basket, increase the quantity.
             if charm in basket[item_id]['items_by_charm_option'].keys():
                 basket[item_id]['items_by_charm_option'][charm] += quantity
+            # If the submitted bag is already in the basket, but not with this
+            # charm combination, add the charm key and the quantity.
             else:
                 basket[item_id]['items_by_charm_option'][charm] = quantity
+        # If the submitted bag is not already in the basket, add the new bag,
+        # charm key and quantity.
         else:
             basket[item_id] = {'items_by_charm_option': {charm: quantity}}
+    # If the submitted bag does not have a charm option.
     else:
+        # If the submitted bag is already in the basket, increase the quantity.
         if item_id in list(basket.keys()):
             basket[item_id] += quantity
+        # If the submitted bag is not already in the basket, add the new bag
+        # and quantity.
         else:
             basket[item_id] = quantity
 
