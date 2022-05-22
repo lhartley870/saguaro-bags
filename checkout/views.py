634a09d9b96b9cb1fdece22a0c5aae85b2b45d71
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-22 17:53:46 +0000

    Add checkout view to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from .forms import OrderForm
+
 
 # Create your views here.
+def checkout(request):
+    """ View to render the checkout page """
+
+    basket = request.session.get('basket', {})
+    # If there is nothing in the basket, render error message
+    # and redirect back to the all bags page.
+    if not basket:
+        messages.error(request,
+                       'There is nothing in your basket at the moment')
+        return redirect(reverse('bags'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
