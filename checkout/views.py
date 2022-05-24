a2a0f5a99770b2fafe84dedf8477ea918ad4062b
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-24 13:36:52 +0000

    Add checkout_sucess view to checkout app

--- checkout/views.py
+++ checkout/views.py
@@ -125,3 +125,25 @@ def checkout(request):
     }
 
     return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+    messages.success(request, f'Order successfully processed! '
+                     f'Your order number is {order_number}. '
+                     f'A confirmation email will be sent to {order.email}.')
+
+    # Clear the shopping basket in session storage.
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
