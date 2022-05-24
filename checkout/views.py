da04973f4aa01d429953e47fbe2ee1b501dddcc9
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-24 12:28:20 +0000

    Add to checkout view to deal with form submission

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.conf import settings
 
@@ -6,7 +6,9 @@ import stripe
 
 from basket.contexts import basket_contents
 
+from products.models import Bag, Charm
 from .forms import OrderForm
+from .models import Order, OrderLineItem
 
 
 # Create your views here.
@@ -16,29 +18,105 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
-    # If there is nothing in the basket, render error message
-    # and redirect back to the all bags page.
-    if not basket:
-        messages.error(request,
-                       'There is nothing in your basket at the moment')
-        return redirect(reverse('bags'))
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
 
-    current_basket = basket_contents(request)
-    total = current_basket['grand_total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+        }
 
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save()
+            for item_id, item_data in basket.items():
+                try:
+                    bag = Bag.objects.get(id=item_id)
+                    # If the item_data is an integer, the bag has no charm
+                    # option
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            bag=bag,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    # If the item_data is not an integer, the bag has a charm
+                    # option
+                    else:
+                        for charm, quantity in item_data[
+                                                        'items_by_charm_option'
+                                                        ].items():
+                            # If a charm has been selected
+                            if charm != 'No charm selected':
+                                charm_id = int(charm)
+                                bag_charm = get_object_or_404(Charm,
+                                                              id=charm_id)
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    bag=bag,
+                                    quantity=quantity,
+                                    bag_charm=bag_charm,
+                                )
+                            # If a charm has not been selected
+                            else:
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    bag=bag,
+                                    quantity=quantity,
+                                )
+                            order_line_item.save()
+                # This should not happen, but in case a bag in the basket
+                # does not exist, an error message is rendered, the order
+                # is deleted and user is returned to the shopping basket page.
+                except Bag.DoesNotExist:
+                    messages.error(request, (
+                        "One of the items in your basket wasn't "
+                        "found in our database. "
+                        "Please call us for assistance!")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_basket'))
+
+            # Save the info to the user's profile if all is well
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
+        else:
+            messages.error(request, ('There was an error with your form. '
+                                     'Please double check your information.'))
+    else:
+        basket = request.session.get('basket', {})
+        # If there is nothing in the basket, render error message
+        # and redirect back to the all bags page.
+        if not basket:
+            messages.error(request,
+                           'There is nothing in your basket at the moment')
+            return redirect(reverse('bags'))
+
+        current_basket = basket_contents(request)
+        total = current_basket['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+        order_form = OrderForm()
+
+    # If stripe public key is missing render error message.
     if not stripe_public_key:
         messages.warning(request, ('Stripe public key is missing. '
                                    'Did you forget to set it in '
                                    'your environment?'))
 
-    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
