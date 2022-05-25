724ed5a10517bf9ecabdf1ec6b8867590a75a841
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-25 17:19:22 +0000

    Add user_profile field to checkout app Order model

--- checkout/models.py
+++ checkout/models.py
@@ -7,10 +7,14 @@ from django.conf import settings
 from django_countries.fields import CountryField
 
 from products.models import Bag, Charm
+from profiles.models import UserProfile
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, editable=False)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True,
+                                     related_name='orders')
     date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
