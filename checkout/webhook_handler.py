3804ac45e4c7a4f1809e33527b23617e01aa73d1
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-25 13:29:55 +0000

    Add to webhook success method-ensure order created

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,11 @@
+import json
+import time
+
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+
+from products.models import Bag, Charm
+from .models import Order, OrderLineItem
 
 
 class StripeWH_Handler:
@@ -20,10 +27,118 @@ class StripeWH_Handler:
         Handle the payment_intent.succeeded webhook from Stripe
         """
         intent = event.data.object
+        # Extract data from the Stripe payment intent object.
+        pid = intent.id
+        basket = intent.metadata.basket
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
+
+        # Clean data in the shipping details.
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        attempt = 1
+        # Check if the order has already been created by the checkout view.
+        while attempt <= 5:
+            try:
+                order = Order.objects.get(
+                    full_name__iexact=shipping_details.name,
+                    email__iexact=billing_details.email,
+                    phone_number__iexact=shipping_details.phone,
+                    country__iexact=shipping_details.address.country,
+                    postcode__iexact=shipping_details.address.postal_code,
+                    town_or_city__iexact=shipping_details.address.city,
+                    street_address1__iexact=shipping_details.address.line1,
+                    street_address2__iexact=shipping_details.address.line2,
+                    county__iexact=shipping_details.address.state,
+                    grand_total=grand_total,
+                )
+                order_exists = True
+                break
+            # Try to find the order 5 times over 5 seconds, in case the
+            # checkout view is slow creating it.
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+        # If the order already exists, return 200 HTTP response to Stripe.
+        if order_exists:
+            return HttpResponse(
+                content=(f'Webhook received: {event["type"]} | SUCCESS: '
+                         'Verified order already in database'),
+                status=200)
+        # If the order does not already exist e.g. because of a user error
+        # during the checkout process, try to create the order.
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                )
+                for item_id, item_data in json.loads(basket).items():
+                    bag = Bag.objects.get(id=item_id)
+                    # If the item_data is an integer, the bag has no charm
+                    # option.
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            bag=bag,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+                    # If the item_data is not an integer, the bag has a charm
+                    # option.
+                    else:
+                        for charm, quantity in item_data[
+                                                        'items_by_charm_option'
+                                                        ].items():
+                            # If a charm has been selected
+                            if charm != 'No charm selected':
+                                charm_id = int(charm)
+                                bag_charm = get_object_or_404(Charm,
+                                                              id=charm_id)
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    bag=bag,
+                                    quantity=quantity,
+                                    bag_charm=bag_charm,
+                                )
+                            # If a charm has not been selected
+                            else:
+                                order_line_item = OrderLineItem(
+                                    order=order,
+                                    bag=bag,
+                                    quantity=quantity,
+                                )
+                            order_line_item.save()
+            # If something goes wrong with trying to create the order,
+            # delete any order created and return a 500 server error
+            # response to Stripe.
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+        # If the order has been created by the webhook handler, return 200
+        # HTTP response to Stripe.
         return HttpResponse(
             content=(f'Webhook received: {event["type"]} | SUCCESS: '
                      'Created order in webhook'),
-            status=200)
+            status=200
+        )
 
     def handle_payment_intent_payment_failed(self, event):
         """
