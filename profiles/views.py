fabe019d72cffe3b5230b6183c47cac015f5d334
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-26 19:31:17 +0000

    Adjust profile view to add form & orders to context

--- profiles/views.py
+++ profiles/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
-    profile = get_object_or_404(UserProfile, user=request.user)
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=user_profile)
+    orders = user_profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
 
     return render(request, template, context)
