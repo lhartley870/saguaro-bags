4a5de85a92409b5d00472f123138e847229b1e55
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-28 15:35:52 +0000

    Update profile view to render error message

--- profiles/views.py
+++ profiles/views.py
@@ -26,8 +26,13 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
+    else:
+        form = UserProfileForm(instance=user_profile)
 
-    form = UserProfileForm(instance=user_profile)
     orders = user_profile.orders.all()
 
     template = 'profiles/profile.html'
