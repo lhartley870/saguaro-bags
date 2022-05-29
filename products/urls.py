65872c065ce5030fde9ac6dea250a52ca3c264ee
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-29 16:13:35 +0000

    Add delete bag path to products app urls.py

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('<int:bag_id>', views.bag_detail, name='bag_detail'),
     path('add/', views.add_bag, name='add_bag'),
     path('edit/<int:bag_id>/', views.edit_bag, name='edit_bag'),
+    path('delete/<int:bag_id>/',
+         views.delete_bag,
+         name='delete_bag'),
 ]
