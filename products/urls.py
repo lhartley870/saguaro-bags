f5ad52f650b887a78a986381de9eabf695c0cfae
lhartley870 <hartleylc@hotmail.co.uk>
2022-05-29 15:50:29 +0000

    Add edit bag path to products app urls.py

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.all_bags, name='bags'),
     path('<int:bag_id>', views.bag_detail, name='bag_detail'),
     path('add/', views.add_bag, name='add_bag'),
+    path('edit/<int:bag_id>/', views.edit_bag, name='edit_bag'),
 ]
